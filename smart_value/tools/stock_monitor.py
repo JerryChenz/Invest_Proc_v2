83abd7112f8479aaa27fac1f105942a1a9fffbcb
Jerry.chen <jerrychen.works@gmail.com>
2023-06-21 19:12:29 +0800

    Updated the models

--- smart_value/tools/stock_monitor.py
+++ smart_value/tools/stock_monitor.py
@@ -214,12 +214,15 @@ class MonitorStock:
         self.exp_value = dash_sheet.range('E14').value
         self.breakeven_price = dash_sheet.range('B17').value
         self.next_buy_price = dash_sheet.range('C35').value
-        self.next_buy_shares = dash_sheet.range('C36').value
+        if dash_sheet.range('E26').value == "Action_Planning":
+            self.next_buy_shares = dash_sheet.range('E28').value
+        else:
+            self.next_buy_shares = dash_sheet.range('C28').value
         self.exp_exit_price = dash_sheet.range('I35').value
         self.worst_case_price = dash_sheet.range('I36').value
         self.lfy_date = dash_sheet.range('E6').value  # date of the last financial year-end
         self.next_review = dash_sheet.range('D6').value
-        self.frd_dividend = dash_sheet.range('I16').value
+        self.frd_dividend = dash_sheet.range('J16').value
         # Current holdings attributes
         self.hold = dash_sheet.range('E26').value == "Action_Planning"
         self.shares_hold = dash_sheet.range('C28').value
